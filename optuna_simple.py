0871be5b9c1530f19bd39a2afa24d21dafe129df
22sawada <sawada-eri459@g.ecc.u-tokyo.ac.jp>
2025-02-27 15:36:33 +0900

    add_data

--- optuna_simple.py
+++ optuna_simple.py
@@ -33,13 +33,17 @@ profiller.dump_stats('./'+dir_name+'/'+file_name+'.prof')
 
 stats = pstats.Stats(profiller)
 function_name = "_pop_waiting_trial_id"
+function_name_1 = "get_all_trials"
+i = 0
 
 sys.stdout = open('./'+dir_name+'/'+file_name+'.txt', 'w')
 for func, data in stats.stats.items():
-    if function_name in func[2]:  # 関数名が一致するものを探す
+    if function_name in func[2] or function_name_1 in func[2]:  # 関数名が一致するものを探す
         ncalls, tottime, percall, cumtime, percall_cum = data
         print(f"関数: {func[2]}")
         print(f"  呼び出し回数: {ncalls}")
         print(f"  合計実行時間 (tottime): {tottime:.6f} 秒")
         print(f"  累積時間 (cumtime): {cumtime:.6f} 秒")
-        break
+        i += 1
+        if i == 2:
+            break
--- script.sh
+++ script.sh
@@ -1,13 +1,13 @@
-mkdir master_without_waiting_trials_100
-mkdir fix_without_waiting_trials_100
+mkdir master_without_waiting_trials_1000
+mkdir fix_without_waiting_trials_1000
 for i in {1..10}
 do
     cd ~/pfn/optuna
     git switch master
     cd ~/pfn/examples
-    python optuna_simple.py 0 100 master_without_waiting_trials_100 $i
+    python optuna_simple.py 0 1000 master_without_waiting_trials_1000 $i
     cd ~/pfn/optuna
     git switch fix/speed-up-pop-waiting-trial-id-1
     cd ~/pfn/examples
-    python optuna_simple.py 0 100 fix_without_waiting_trials_100 $i
+    python optuna_simple.py 0 1000 fix_without_waiting_trials_1000 $i
 done
