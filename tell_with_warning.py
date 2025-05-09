f8b56caa9d6830276b0bf9b1fce148c593a43e66
Eri Sawada <esawada2003@gmail.com>
2025-05-09 15:14:27 +0900

    add data

--- tell_with_warning.py
+++ tell_with_warning.py
@@ -24,17 +24,27 @@ profiller.dump_stats('./'+dir_name+'/'+file_name+'.prof')
 
 stats = pstats.Stats(profiller)
 function_name = "_run_trial"
-function_name_1 = "get_all_trials"
+# function_name_1 = "_tell_with_warning"
 i = 0
+
+cumtime_global = 0
 
 sys.stdout = open('./'+dir_name+'/'+file_name+'.txt', 'w')
 for func, data in stats.stats.items():
-    if function_name in func[2] or function_name_1 in func[2]:
+    if function_name in func[2]:
         ncalls, tottime, percall, cumtime, percall_cum = data
         print(f"関数: {func[2]}")
         print(f"  呼び出し回数: {ncalls}")
         print(f"  合計実行時間 (tottime): {tottime:.6f} 秒")
         print(f"  累積時間 (cumtime): {cumtime:.6f} 秒")
+        cumtime_global += cumtime
         i += 1
         if i == 2:
             break
+
+sys.stdout.close()
+
+f = open('./'+dir_name+'/'+'sum.txt', 'a')
+f.write(str(cumtime_global))
+f.write('\n')
+f.close()
