1f48ea1ce5df62172fe8add6d89d7819809613fa
Eri Sawada <esawada2003@gmail.com>
2025-05-15 20:47:09 +0900

    tell_with_warning

--- tell_with_warning.py
+++ tell_with_warning.py
@@ -1,11 +1,36 @@
-import optuna
-import cProfile
-import sys
-import pstats
+# import optuna
+# import cProfile
+# import sys
+# import pstats
 
-args = sys.argv
-dir_name = args[1]
-file_name = args[2]
+# def objective(trial: optuna.Trial) -> float:
+#     x = trial.suggest_float("x", -1, 1)
+#     y = trial.suggest_int("y", -1, 1)
+#     return x**2 + y
+
+
+# study = optuna.create_study()
+# cProfile.run("study.optimize(objective, n_trials=1000)", filename="pr6079.stats")
+# # cProfile.run("study.optimize(objective, n_trials=1000)", filename="master.stats")
+
+import warnings
+
+# 全てのWarningを表示
+warnings.simplefilter("always")
+import optuna
+import logging
+
+class ListHandler(logging.Handler):
+    def emit(self, record):
+        log_capture.append(self.format(record))
+
+_logger = logging.getLogger("optuna")
+log_capture = []
+handler = ListHandler()
+formatter = logging.Formatter('%(levelname)s: %(message)s')
+handler.setFormatter(formatter)
+_logger.addHandler(handler)
+_logger.setLevel(logging.WARNING)
 
 def objective(trial: optuna.Trial) -> float:
     x = trial.suggest_float("x", -1, 1)
@@ -14,37 +39,8 @@ def objective(trial: optuna.Trial) -> float:
 
 
 study = optuna.create_study()
-# cProfile.run("study.optimize(objective, n_trials=500)", filename="pr6078_2.stats")
-# cProfile.run("study.optimize(objective, n_trials=500)", filename="master.stats")
-
-profiller = cProfile.Profile()
-profiller.run('study.optimize(objective, n_trials=500)')
-
-profiller.dump_stats('./'+dir_name+'/'+file_name+'.prof')
-
-stats = pstats.Stats(profiller)
-function_name = "_run_trial"
-# function_name_1 = "_tell_with_warning"
-i = 0
-
-cumtime_global = 0
-
-sys.stdout = open('./'+dir_name+'/'+file_name+'.txt', 'w')
-for func, data in stats.stats.items():
-    if function_name in func[2]:
-        ncalls, tottime, percall, cumtime, percall_cum = data
-        print(f"関数: {func[2]}")
-        print(f"  呼び出し回数: {ncalls}")
-        print(f"  合計実行時間 (tottime): {tottime:.6f} 秒")
-        print(f"  累積時間 (cumtime): {cumtime:.6f} 秒")
-        cumtime_global += cumtime
-        i += 1
-        if i == 2:
-            break
-
-sys.stdout.close()
-
-f = open('./'+dir_name+'/'+'sum.txt', 'a')
-f.write(str(cumtime_global))
-f.write('\n')
-f.close()
+frozen_trial = optuna.study._optimize._run_trial(study, lambda _: float("nan"), catch=())
+for log in log_capture:
+    print(f"Caught log: {log}")
+# study = optuna.create_study(sampler=optuna.samplers.RandomSampler())
+# study.optimize(objective, n_trials=10000)
