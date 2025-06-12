2636dfd798372eff482720343ab0d7e523eaf58e
Eri Sawada <esawada2003@gmail.com>
2025-06-12 18:48:46 +0900

    add data

--- script.sh
+++ script.sh
@@ -1,14 +1,14 @@
 mkdir master_1000
 mkdir fix_1000
 
-for i in {1..10}
+for i in {1..5}
 do
     cd ~/pfn/optuna
     git switch master
     cd ~/pfn/examples
     python tell_with_warning.py master_1000 $i
     cd ~/pfn/optuna
-    git switch remove-get-trial-in-tell-with-warning
+    git switch remove-the-last-get-trial-in-tell-with-warning
     cd ~/pfn/examples
     python tell_with_warning.py fix_1000 $i
 done
--- tell_with_warning.py
+++ tell_with_warning.py
@@ -1,36 +1,52 @@
-# import optuna
-# from optuna.trial import TrialState
-# import optunahub
+import optuna
+import optunahub
+import cProfile
 
-# storage = optuna.storages.RDBStorage(
-#     url="sqlite:///:memory:",
-#     engine_kwargs={"pool_size": 20, "connect_args": {"timeout": 10}},
-# )
+import sys
+import pstats
 
-# def objective(trial: optuna.Trial) -> float:
-#     x = trial.suggest_float("x", -1, 1)
-#     y = trial.suggest_int("y", -1, 1)
-#     if y == 0:
-#         trial.storage.set_trial_state_values(
-#             trial._trial_id, TrialState.COMPLETE, [0.0]
-#         )
-#     return x**2 + y
+args = sys.argv
+dir_name = args[1]
+file_name = args[2]
 
-# study = optuna.create_study(sampler=optuna.samplers.RandomSampler(), storage=storage)
-# study.optimize(objective, n_trials=10)
+storage = optuna.storages.RDBStorage(
+    url="sqlite:///:memory:",
+    engine_kwargs={"pool_size": 20, "connect_args": {"timeout": 10}},
+)
 
+def objective(trial: optuna.Trial) -> float:
+    x = trial.suggest_float("x", -1, 1)
+    y = trial.suggest_int("y", -1, 1)
+    return x**2 + y
 
-import os
+study = optuna.create_study(sampler=optuna.samplers.RandomSampler(), storage=storage)
 
-from optuna.storages import run_grpc_proxy_server
-from optuna.storages.journal import JournalFileBackend
-from optuna.storages.journal import JournalStorage
+profiller = cProfile.Profile()
+profiller.run("study.optimize(objective, n_trials=500)")
 
+profiller.dump_stats('./'+dir_name+'/'+file_name+'.prof')
+stats = pstats.Stats(profiller)
+function_name = "_run_trial" 
+# function_name_1 = "_tell_with_warning"
+i = 0
+cumtime_global = 0
 
-try:
-    os.remove("test-grpc.log")
-except FileNotFoundError:
-    pass
-storage = JournalStorage(JournalFileBackend("test-grpc.log"))
-run_grpc_proxy_server(storage, host="localhost", port=13000)
+sys.stdout = open('./'+dir_name+'/'+file_name+'.txt', 'w')
+for func, data in stats.stats.items():
+    if function_name in func[2]:
+        ncalls, tottime, percall, cumtime, percall_cum = data
+        print(f"関数: {func[2]}")
+        print(f"  呼び出し回数: {ncalls}")
+        print(f"  合計実行時間 (tottime): {tottime:.6f} 秒")
+        print(f"  累積時間 (cumtime): {cumtime:.6f} 秒")
+        cumtime_global += cumtime
+        i += 1
+        if i == 2:
+            break
 
+sys.stdout.close()
+
+f = open('./'+dir_name+'/'+'sum.txt', 'a')
+f.write(str(cumtime_global))
+f.write('\n')
+f.close()
