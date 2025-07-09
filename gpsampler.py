0efdc43512c42dd0b469a742788c9f76336357a6
Eri Sawada <esawada2003@gmail.com>
2025-07-09 10:46:46 +0900

    add empty sqlite3

---
+++ gpsampler.py
@@ -0,0 +1,35 @@
+import optuna
+
+
+def objective(trial: optuna.Trial) -> float:
+    x = trial.suggest_float("x", -5, 5)
+    y = trial.suggest_float("y", -5, 5)
+    return x**2 + y**2
+
+
+def multi_objective(trial: optuna.Trial) -> tuple[float, float]:
+    x = trial.suggest_float("x", -5, 5)
+    y = trial.suggest_float("y", -5, 5)
+    return x**2 + y**2, (x - 2)**2 + (y - 2)**2
+
+
+def constraints(trial: optuna.trial.FrozenTrial) -> tuple[float, float]:
+    x = trial.params["x"]
+    y = trial.params["y"]
+    return (x - 2, y - 2)
+
+
+mode = ["single", "multi", "constr"][1]
+if mode == "single":
+    study = optuna.create_study(sampler=optuna.samplers.GPSampler(seed=42))
+    obj_func = objective
+elif mode == "multi":
+    study = optuna.create_study(sampler=optuna.samplers.GPSampler(seed=0), directions=["minimize"]*2)
+    obj_func = multi_objective
+elif mode == "constr":
+    study = optuna.create_study(sampler=optuna.samplers.GPSampler(seed=42, constraints_func=constraints))
+    obj_func = objective
+
+study.optimize(obj_func, n_trials=20)
+trials = study.trials
+print((trials[-1].datetime_complete - trials[0].datetime_start).total_seconds())
--- test_cmaes.py
+++ test_cmaes.py
@@ -2,28 +2,31 @@ import optuna
 import optunahub
 import cProfile
 
-import sys
-import pstats
-import time
+from multiprocessing import Pool
+from optuna.storages import JournalStorage
+from optuna.storages.journal import JournalFileBackend
+# import sys
+# import pstats
+# import time
 
-args = sys.argv
-dir_name = args[1]
-file_name = args[2]
+# args = sys.argv
+# dir_name = args[1]
+# file_name = args[2]
 
 # storage = optuna.storages.RDBStorage(
 #     url="sqlite:///:memory:",
 #     engine_kwargs={"pool_size": 20, "connect_args": {"timeout": 10}},
 # )
-if dir_name == 'master_5000':
-    module = optunahub.load_module(
-        package="samplers/restart_cmaes",
-    )
-else:
-    module = optunahub.load_module(
-        package="samplers/restart_cmaes",
-        repo_owner="sawa3030",
-        ref="fix/use-in-memory-in-restartcmaessampler",
-    )
+# if dir_name == 'master_5000':
+#     module = optunahub.load_module(
+#         package="samplers/restart_cmaes",
+#     )
+# else:
+module = optunahub.load_module(
+    package="samplers/restart_cmaes",
+    repo_owner="sawa3030",
+    ref="fix/use-in-memory-in-restartcmaessampler-1",
+)
 RestartCmaEsSampler = module.RestartCmaEsSampler
 
 
@@ -34,13 +37,23 @@ def objective(trial: optuna.Trial) -> float:
 
 
 # sampler = RestartCmaEsSampler()  # CMA-ES without restart (default)
-sampler = RestartCmaEsSampler(restart_strategy="ipop")  # IPOP-CMA-ES
+sampler = RestartCmaEsSampler(restart_strategy="ipop", seed = 42, store_optimizer_state_in_storage = False)  # IPOP-CMA-ES
 # sampler = RestartCmaEsSampler(restart_strategy="bipop")  # BIPOP-CMA-ES
-study = optuna.create_study(sampler=sampler)
-start = time.time()
-study.optimize(objective, n_trials=200)
-end = time.time()
-print(f"Total time taken: {end - start:.2f} seconds")
+# study = optuna.create_study(sampler=sampler)
+# start = time.time()
+def run_optimization(_):
+    study = optuna.create_study(
+        study_name="journal_storage_multiprocess",
+        storage=JournalStorage(JournalFileBackend(file_path="./journal.log")),
+        load_if_exists=True,
+        sampler=sampler)
+    study.optimize(objective, n_trials=10)
+    print(len(sampler._optimizer_states_by_trial))
+if __name__ == "__main__":
+    with Pool(processes=4) as pool:
+        pool.map(run_optimization, range(3))
+# end = time.time()
+# print(f"Total time taken: {end - start:.2f} seconds")
 # profiller = cProfile.Profile()
 # profiller.run("study.optimize(objective, n_trials=200)")
 
@@ -66,6 +79,6 @@ print(f"Total time taken: {end - start:.2f} seconds")
 
 # sys.stdout.close()
 
-f = open('./'+dir_name+'/'+'sum.txt', 'a')
-f.write(str(end - start) + '\n')
-f.close()
+# f = open('./'+dir_name+'/'+'sum.txt', 'a')
+# f.write(str(end - start) + '\n')
+# f.close()
